f8fed24634c93c218ac9e3e40b2860e1cd6588b9
mbrau22s <marvin.braun1@smail.inf.h-brs.de>
2023-01-15 21:40:13 +0100

    Updating the Project

--- real stuff/linkmethods.py
+++ real stuff/linkmethods.py
@@ -1,16 +1,15 @@
 import glob
 import sys
-
 import pandas as pd
-import psutil
 import requests
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 from datetime import datetime
 from urllib import parse
 from bs4 import BeautifulSoup
 from collections import Counter
-import numpy as np
-import os
 
 URL = "https://wikimedia.org/api/rest_v1/"
 HEADERS = {"Accept": "application/json", "user-agent": "marvin.braun1@smail.inf.h-brs.de"}
@@ -54,59 +53,14 @@ def get_pageviews(article: str,
 
 
 def get_views(article: str):
-    data = get_pageviews(article, project="de.wikipedia.org")
-    views = []
-    for entry in data:
-        views.append((entry['views']))
+    try:
+        data = get_pageviews(article)
+        views = []
+        for entry in data:
+            views.append((entry['views']))
+    except:
+        print("No Data Availible for:", article)
     return views
-
-
-def get_forward_links(article: str):
-    S = requests.Session()
-    URL = "https://de.wikipedia.org/w/api.php"
-
-    PARAMS = {
-        "action": "query",
-        "titles": article,
-        "gpllimit": "max",
-        "format": "json",
-        "generator": "links"
-
-    }
-    R = S.get(url=URL, params=PARAMS)
-    DATA = R.json()
-    PAGES = DATA['query']['pages']
-    titels = []
-
-    for page in PAGES.values():
-        if 'missing' not in page:
-            titels.append(page['title'])
-
-    return titels
-
-
-def get_back_links(article: str):
-    S = requests.Session()
-    URL = "https://de.wikipedia.org/w/api.php"
-
-    PARAMS = {
-        "action": "query",
-        "bltitle": article,
-        "plcontinue": True,
-        "bllimit": "max",
-        "format": "json",
-        "list": "backlinks",
-    }
-    data = get_pageviews(article)
-    R = S.get(url=URL, params=PARAMS)
-    DATA = R.json()
-    BACKLINKS = DATA["query"]["backlinks"]
-    blinks = []
-
-    for b in BACKLINKS:
-        blinks.append(b["title"])
-
-    return blinks
 
 
 def count_links(article: str):
@@ -126,7 +80,7 @@ def get_target(target: str):
         write_backlinks_tocsv(target)
     except:
         pass
-    path = r'c:\Users\Marvin\PycharmProjects\pythonProject/real stuff/datafiles/' + target + '/' + target + '.csv'
+    path = r'datafiles/' + target + '/' + target + '.csv'
     data = pd.read_csv(path)
     df = pd.DataFrame(data)[["timestamp", "views"]]
     df["timestamp"] = pd.to_numeric(df["timestamp"])
@@ -135,7 +89,7 @@ def get_target(target: str):
 
 
 def get_backlink_views(target, ref_df):
-    path = r'c:\Users\Marvin\PycharmProjects\pythonProject/real stuff/datafiles/' + target + '/backlinksdata'
+    path = r'datafiles/' + target + '/backlinksdata'
     csv_files = glob.glob(os.path.join(path, "*.csv"))
     flatten_views = []
 
@@ -150,15 +104,20 @@ def get_backlink_views(target, ref_df):
 
 
 def get_backlink_strings(article):
-    path = r'c:\Users\Marvin\PycharmProjects\pythonProject/real stuff/datafiles/' + article + '/backlinksdata'  # make path a global variable
+    path = r'datafiles/' + article + '/backlinksdata'  # make path a global variable
     extension = 'csv'
     os.chdir(path)
     result = glob.glob('*.{}'.format(extension))
     cropped_result = [r[0:-4] for r in result]
     return cropped_result
-def get_links(article: str, prop: str):
 
-    "props = links or linkshere"
+
+def get_links(article: str, prop: str):
+    '''
+    props:
+    linkshere = Backlinks
+    links = forwardlinks
+    '''
 
     if prop == "links":
         limit = "pllimit=max"
@@ -168,7 +127,8 @@ def get_links(article: str, prop: str):
         con = "lhcontinue="
 
     titles = []
-    data = requests.get(f"https://de.wikipedia.org/w/api.php?action=query&format=json&titles={article}&prop={prop}&{limit}").json()
+    data = requests.get(
+        f"https://de.wikipedia.org/w/api.php?action=query&format=json&titles={article}&prop={prop}&{limit}").json()
     data_continue = data
 
     data = to_links(data, prop)
@@ -177,13 +137,15 @@ def get_links(article: str, prop: str):
 
     while list(data_continue.keys())[0] == "continue":
         data_continue = data_continue["continue"]
-        data_continue = data_continue[con[0:len(con)-1]]
-        data_continue = requests.get(f"https://de.wikipedia.org/w/api.php?action=query&format=json&titles={article}&prop={prop}&{limit}&{con}{data_continue}").json()
+        data_continue = data_continue[con[0:len(con) - 1]]
+        data_continue = requests.get(
+            f"https://de.wikipedia.org/w/api.php?action=query&format=json&titles={article}&prop={prop}&{limit}&{con}{data_continue}").json()
         data_list = to_links(data_continue, prop)
         for i in data_list:
             titles.append(i["title"])
 
     return titles
+
 
 def to_links(data: dict, prop: str):
     data = data["query"]
@@ -193,29 +155,39 @@ def to_links(data: dict, prop: str):
 
 
 def write_backlinks_tocsv(target: str):
+    """
+    Filtering the incoming list to filter out unwanted links to unnecessary Websites that have no Data or low amounts,
+    then saving them to a relative path in the directory.
+    """
+
     if not isinstance(target, str):
         raise TypeError('Target has to be a string')
     else:
-        data = get_links(target,"linkshere")
-        new = [k for k in data if not 'Benutzer' in k]
+        new0 = get_links(target, "linkshere")
+        new = [k for k in new0 if not 'Benutzer' in k]
         new1 = [k for k in new if not 'Diskussion' in k]
         new2 = [k for k in new1 if not 'Wikipedia' in k]
         new3 = [k for k in new2 if not '/' in k]
+        new4 = [k for k in new3 if not 'Kategorie' in k]
         pathindir = "datafiles/" + target
         pathofblinkdata = "datafiles/" + target + "/backlinksdata"
-        os.mkdir(pathindir)
-        os.mkdir(pathofblinkdata)
+        try:
+            os.mkdir(pathindir)
+            os.mkdir(pathofblinkdata)
+        except:
+            print("Directory already exists, or the path is wrong")
+            pass
         maindf = pd.DataFrame(get_pageviews(target))
-        maindf.to_csv(pathindir+ '/' + target + '.csv', index=False)
-        for entry in new3:
+        maindf.to_csv(pathindir + '/' + target + '.csv', index=False)
+        for entry in new4:
             df = pd.DataFrame(get_pageviews(entry))
-            if len(df) < (len(maindf)/2):
+            if len(df) < (len(maindf) / 2):
                 pass
             else:
                 df.to_csv(pathofblinkdata + '/' f'{entry}' + '.csv', index=False)
 
 
-def filterbacklinks(listtofilter: list):
+def filterlinks(listtofilter: list):
     if not isinstance(listtofilter, list):
         raise TypeError('Target has to be a list containing all Backlinks from one page')
     else:
@@ -223,59 +195,28 @@ def filterbacklinks(listtofilter: list):
         new1 = [k for k in new if not 'Diskussion' in k]
         new2 = [k for k in new1 if not 'Wikipedia' in k]
         new3 = [k for k in new2 if not '/' in k]
-        return new3
+        new4 = [k for k in new3 if not 'Kategorie' in k]
+        return new4
 
 
-
-############################################################################################################
-if __name__ == '__main__':
-
-    views = []
-    timestamp = []
-    year = []
-
+def plottimeseries(article: str):
     # get data from one article
-    data = get_pageviews("Meisen")
-
-    # Get the timestamp of the first set of Data
-
+    data = get_pageviews(article)
+    views = []
     for entry in data:
         views.append((entry['views']))
-
+    # Plotting the Timeseries
     x = np.linspace(2015.5, 2022.5, num=len(views))
     plt.plot(x, views)
     plt.xlabel("Time")
     plt.ylabel("Views")
-    plt.title("Meisen")
+    plt.title(article)
     plt.show()
 
-    write_backlinks_tocsv("Wald-_und_Baumgrenze")
 
-    '''
-    #Getting all the Filtered Backlinks and then Writing them to an CSV for easier access
-    for entry in new5:
-        df = pd.DataFrame(get_pageviews(entry))
-        if len(df) < 500:
-            pass
-        else:
-            df.to_csv(path + '/' f'{entry}' + '.csv', index=False)
-            print(df)
-
-    
-    filterdf = pd.DataFrame(dataB)
-    filterdf.columns = ["Backlinks"]
-    filterdf2 = filterdf[filterdf["Backlinks"].str.contains("Benutzer")]
-    filterdf3 = filterdf[filterdf["Backlinks"].str.contains("Diskussion")]
-    print(filterdf2)
-    print(filterdf3)
-    print("b4",len(filterdf))
-    print(filterdf)
-    print("after", len(filterdf))
-    print(len(filterdf)-len(filterdf2))
-    '''
+############################################################################################################
+if __name__ == '__main__':
+    plottimeseries("Meisen")
+    plottimeseries("Kurvendiskussion")
+    plottimeseries("Lyrik")
     sys.exit()
-
-# Can remove unusefull links via:
-# dataB.remove("Magdeburger Straßen/M")
-# dataB.remove("Benutzer Diskussion:Nina/Archiv2")
-# dataB.remove("Benutzer:Atamari/Liste der Vögel in Gambia")
--- real stuff/zeitreihenanalyse.py
+++ real stuff/zeitreihenanalyse.py
@@ -1,68 +1,23 @@
-from datetime import datetime
-from itertools import zip_longest
-from statistics import mean
 import pandas as pd
 import numpy as np
+
 from matplotlib import pyplot as plt
 from matplotlib import lines as mlines
 from pandas.core import frame
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
-from linkmethods import get_pageviews, get_back_links, get_links
-from testplots import analysis
-
-
-def analyse(mainpage):
-    mainviews = pageviewget(mainpage)
-    backlinksMainview = get_back_links(mainpage)
-    summe = []
-
-    for entry in mainviews:
-        summe.append(0)
-
-    for entry in backlinksMainview:
-        views = pageviewget(entry)
-        differenceViews = [abs(m - km) for m, km in zip(mainviews, views)]
-        newDifference = difference(mainviews, views, differenceViews)
-        i = 1.00
-
-        diff = [abs(s - m) for s, m in zip(mainviews, views)]
-        summe = [(km + bm) for km, bm in zip(summe, newDifference)]
-
-    return summe
-
-
-def pageviewget(article):
-    views = []
-    data = []
-
-    try:
-        data = get_pageviews(article, project="de.wikipedia.org")
-        for entry in data:
-            views.append((entry['views']))
-    except:
-        pass
-
-    return views
-
-
-def difference(views, views2, diffViews2):
-    diffViews = (sum(diffViews2)) / len(views)
-
-    for i in range(0, len(views2)):
-        views2[i] = views2[i] + diffViews
-    return views2
+from linkmethods import get_views,get_links
 
 
 def lineareRegression(article):
-    mainviews = pageviewget(article)
+    mainviews = get_views(article)
     backlinksMainview = get_links(article, "linkshere")
     df = pd.DataFrame()
     df2 = pd.DataFrame()
     len1 = len(mainviews)
 
     for entry in backlinksMainview:  # for every backlink from the mainpage
-        views = pageviewget(entry)  # get the views
+        views = get_views(entry)  # get the views
         len2 = len(views)
         if len2 < len1:  # then compare if they have the same amount of entries
             diff = len1 - len2
@@ -70,9 +25,6 @@ def lineareRegression(article):
                 views.append(0)
 
         df.insert(loc=0, column=entry, value=views)
-
-    data = get_pageviews(article, project="de.wikipedia.org")
-    data2 = analysis(data)
 
     x = df
     y = mainviews
@@ -145,13 +97,12 @@ def lineareRegression(article):
 
 
 def logisticRegression(article):
-    mainviews = pageviewget(article)
+    mainviews = get_views(article)
     backlinksMainview = get_links(article, "linkshere")
     df = pd.DataFrame()
     len1 = len(mainviews)
-
     for entry in backlinksMainview:
-        views = pageviewget(entry)
+        views = get_views(entry)
         len2 = len(views)
         if len2 < len1:
             diff = len1 - len2
@@ -159,9 +110,6 @@ def logisticRegression(article):
                 views.append(0)
 
         df.insert(loc=0, column=entry, value=views)
-
-    data = get_pageviews(article, project="de.wikipedia.org")
-    data2 = analysis(data)
 
     x = df
     y = mainviews
@@ -185,7 +133,6 @@ def logisticRegression(article):
     x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                         test_size=0.3,
                                                         random_state=None)
-
 
     x1 = np.linspace(2015, 2022.5, num=len(yhat))
 
@@ -211,117 +158,6 @@ def logisticRegression(article):
 
     print("SMAPE Log-Lin Regression: ", differenceViews.mean())
 
-
-def exponentielleRegression(article):
-    mainviews = pageviewget(article)
-    backlinksMainview = get_back_links(article)
-    first20 = backlinksMainview[0:20]
-    df = pd.DataFrame()
-    df2 = pd.DataFrame()
-    len1 = len(mainviews)
-    print(len(mainviews))
-
-    for entry in backlinksMainview:  # for every backlink from the mainpage
-        views = pageviewget(entry)  # get the views
-        len2 = len(views)
-        print(len(views))
-        insert = []
-
-        if len2 < len1:  # then compare if they have the same amount of entries
-            diff = len1 - len2
-            print(diff)
-            for i in range(0, diff):
-                views.append(0)
-
-        for e in views:
-            if e != 0:
-                insert.append(np.log(e))
-            if e == 0:
-                insert.append(0)
-
-        df.insert(loc=0, column=entry, value=insert)
-
-    data = get_pageviews(article, project="de.wikipedia.org")
-    data2 = analysis(data)
-
-    x = df
-    y = mainviews
-
-    model = LinearRegression()
-    model.fit(x, y)
-
-    intercept = model.intercept_
-    slope = model.coef_
-
-    print("lenSlope", len(slope))
-    return intercept, slope
-
-
-def berechneDaten(alpha, beta, entrys):
-    summe = []
-    for entry in entrys[0:20]:
-        for views in range(0, len(entry)):
-            summe.append((alpha * views + beta))
-
-    return summe
-
-
-def berechneSumme(alpha, beta, summe, scr, abw):
-    i = 0
-    print(max(summe))
-
-    for entry in summe[0: len(summe)]:
-        if entry > 250 * abw:
-            entry = mean(summe)
-            print(entry)
-
-        summe[i] = (alpha + beta * entry) * (1 - scr)
-
-        i += 1
-    return summe
-
-
-def berechneSumme2(alpha, beta, backlinks):
-    i = 0
-    print("Maxbacklinks", max(backlinks))
-    print("lenofbacklinks", len(backlinks))
-    initViews = pageviewget(backlinks[0])
-
-    summe = []
-    for x in range(0, len(initViews)):
-        summe.append(0)
-
-    entrys = []
-    for y in range(0, len(initViews)):
-        entrys.append(0)
-
-    for entry in backlinks[0: len(backlinks)]:
-        views = pageviewget(entry)
-
-        for view in views[0: len(views)]:
-            print("BETAAAAA", beta[i])
-            print("ENTRYYYSSSS", len(entrys))
-            print("VIIIEEWWWWSSSS", len(views))
-
-            if len(entrys) > len(views):
-                diff = len(entrys) - len(views)
-                for i in range(0, diff):
-                    views.append(0)
-
-            print("KEYYYYYYYYYYYY", view)
-            entrys = [(e + beta[i] * v) for e, v in zip_longest(entrys, views)]
-
-        summe = [(s + e + alpha) for s, e in zip_longest(summe, entrys)]
-        i += 1
-    return summe
-
-
-def abweichungsfaktor(summe, mainview):
-    abw = [abs(s1 - m) for s1, m in zip(summe, mainview)]
-    abw1 = (sum(abw)) / len(abw)
-    return abw1
-
-
 def smape(target, forecast):
     if type(target) == pd.core.frame.DataFrame:
         target = target.values
@@ -335,198 +171,7 @@ def smape(target, forecast):
     return smape
 
 
-def MAPE(target, predicted):
-    def mape(actual, pred):
-        actual, pred = np.array(actual), np.array(pred)
-        return (np.abs((actual - pred) / actual)) * 100
-
-
 if __name__ == '__main__':
-    article = "Vögel"
+    article = "Bundesrat_(Deutschland)"
     lineareRegression(article)
     logisticRegression(article)
-
-
-
-'''
-alpha, beta = lineareRegression("Deutschland")
-
-alpha, beta = lineareRegression("Lineare Optimierung")
-
-alpha, beta = lineareRegression("Weihnachten")
-
-alpha, beta = lineareRegression("Köln")
-
-alpha, beta = lineareRegression("Großer Panda")
-
-
-mainviews = pageviewget("Meisen")
-backlinksMainview = linksAnalysis.get_back_links("Meisen")
-first20entrys = backlinksMainview[0:20]
-summe1 = analyse("Meisen")
-abw2 = abweichungsfaktor(summe1, mainviews)
-su = berechneSumme(alphaMeisen, betaMeisen, summe1, score, abw2)
-
-x1 = np.linspace(2015, 2022.5, num=len(su))
-plt.plot(x1, su)
-print("AAAAAAAAAAAAAAAAAAAAAA", len(su))
-print("BBBBBBBBBBBBBBBBB", len(mainviews))
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Meisen Regression")
-plt.show()
-
-x2 = np.linspace(2015, 2022.5, num=len(mainviews))
-plt.plot(x2, mainviews)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Meisen normal")
-plt.show()
-
-##############
-
-mainviews4 = pageviewget("Meisen")
-backlinksMainview4 = linksAnalysis.get_back_links("Meisen")
-first20entrys4 = backlinksMainview4[0:20]
-summe4 = analyse("Meisen")
-abw4 = abweichungsfaktor(summe4, mainviews)
-su4 = berechneSumme2(a, b, first20entrys4)
-
-x1 = np.linspace(2015, 2022.5, num=len(su4))
-plt.plot(x1, su4)
-plt.xlabel("Time")
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Meisen Regression HILFE")
-plt.show()
-
-x2 = np.linspace(2015, 2022.5, num=len(mainviews))
-plt.plot(x2, mainviews4)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Meisen normal HILFE")
-plt.show()
-
-#######
-
-mainviews2 = pageviewget("Schmetterlinge")
-backlinksMainview2 = linksAnalysis.get_back_links("Schmetterlinge")
-first20entrys2 = backlinksMainview2[0:20]
-summe2 = analyse("Schmetterlinge")
-abw2 = abweichungsfaktor(summe1, mainviews)
-su2 = berechneSumme(alpha2, beta2, summe2, score2, abw2)
-
-x3 = np.linspace(2015, 2022.5, num=len(su2))
-plt.plot(x3, su2)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Schmetterlinge Regression")
-plt.show()
-
-x4 = np.linspace(2015, 2022.5, num=len(mainviews2))
-plt.plot(x4, mainviews2)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Schmetterlinge normal")
-plt.show()
-
-su.append(0)
-su = pd.DataFrame(su, columns=['predicted'])
-mw = pd.DataFrame(mainviews, columns=['views'])
-print("type su", su)
-print("type mv", mw)
-sm = smape(mw, su)
-print("mape ier", MAPE(mw['views'], su['predicted']))
-sm['mape'] = MAPE(mw['views'], su['predicted'])
-print("smape hier lol", sm.mean())
-
-x1 = np.linspace(2015, 2022.5, num=len(s))
-x2 = np.linspace(2015, 2022.5, num=len(mainviews))
-plt.plot(x1, s)
-plt.plot(x2, mainviews)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Meisen Regression")
-plt.show()
-
-mainviews2 = pageviewget("Deutschland")
-backlinksMainview2 = linksAnalysis.get_back_links("Deutschland")
-first20entrys2 = backlinksMainview2[0:20]
-s2 = berechneDaten(alphaDeutschland, betaDeutschland, first20entrys2)
-
-x1 = np.linspace(2015, 2022.5, num=len(s2))
-x2 = np.linspace(2015, 2022.5, num=len(mainviews2))
-plt.plot(x1, s2)
-plt.plot(x2, mainviews2)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("Deutschland Regression")
-plt.show()
-
-viewsMeisen = pageviewget("Meisen")
-viewsKohlmeisen = pageviewget("Kohlmeise")
-viewsBlaumeisen = pageviewget("Blaumeise")
-viewsAmsel = pageviewget("Amsel")
-viewsTannenmeisen = pageviewget("Tannenmeise")
-viewsSchwanzmeisen = pageviewget("Schwanzmeise")
-
-differenceKohlmeisen = [abs(m - km) for m, km in zip(viewsMeisen, viewsKohlmeisen)]
-differenceBlaumeisen = [abs(m - km) for m, km in zip(viewsMeisen, viewsBlaumeisen)]
-differenceAmsel = [abs(m - km) for m, km in zip(viewsMeisen, viewsAmsel)]
-differenceTannenmeisen = [abs(m - km) for m, km in zip(viewsMeisen, viewsTannenmeisen)]
-differenceSchwanzmeisen = [abs(m - km) for m, km in zip(viewsMeisen, viewsSchwanzmeisen)]
-
-differenceKohlmeisen = difference(viewsMeisen, viewsKohlmeisen, differenceKohlmeisen)
-differenceBlaumeisen = difference(viewsMeisen, viewsBlaumeisen, differenceBlaumeisen)
-differenceAmsel = difference(viewsMeisen, viewsAmsel, differenceAmsel)
-differenceTannenmeisen = difference(viewsMeisen, viewsTannenmeisen, differenceTannenmeisen)
-differenceSchwanzmeisen = difference(viewsMeisen, viewsSchwanzmeisen, differenceSchwanzmeisen)
-
-
-x = np.linspace(2015, 2022.5, num=len(viewsKohlmeisen))
-plt.plot(x, viewsKohlmeisen)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("difference + alle views zusammen")
-plt.show()
-print("Max views", max(viewsMeisen))
-
-
-summe = [(0.055*km + 0.055*bm) for km, bm in zip(differenceKohlmeisen, differenceBlaumeisen)]
-summe = [(s + 0.055*a)for s, a in zip(summe, differenceAmsel)]
-summe = [(s + 0.055*tm)for s, tm in zip(summe, differenceTannenmeisen)]
-summe = [(s + 0.055*sm)for s, sm in zip(summe, differenceSchwanzmeisen)]
-
-diff = [abs(s-m)for s, m in zip(summe, viewsMeisen)]
-
-x = np.linspace(2015, 2022.5, num=len(summe))
-plt.plot(x, summe)
-plt.plot(x, viewsMeisen)
-plt.plot(x, diff)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("difference + alle views zusammen")
-plt.show()
-
-summe2 = [(0.08*km + 0.08*bm) for km, bm in zip(differenceKohlmeisen, differenceBlaumeisen)]
-summe2 = [(s + 0.08*a)for s, a in zip(summe2, differenceAmsel)]
-summe2 = [(s + 0.08*tm)for s, tm in zip(summe2, differenceTannenmeisen)]
-summe2 = [(s + 0.08*sm)for s, sm in zip(summe2, differenceSchwanzmeisen)]
-
-diff2 = [abs(s-m)for s, m in zip(summe2, viewsMeisen)]
-
-x = np.linspace(2015, 2022.5, num=len(summe2))
-plt.plot(x, summe2)
-plt.plot(x, viewsMeisen)
-plt.plot(x, diff2)
-plt.xlabel("Time")
-plt.ylabel("Views")
-plt.title("difference + alle views zusammen")
-plt.show()
-
-diff = sum(diff) / len(viewsMeisen)
-medianMeise = sum(viewsMeisen) / len(viewsMeisen)
-print(diff)
-print(medianMeise)
-
-'''
